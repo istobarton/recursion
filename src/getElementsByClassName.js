f78c073dc497e998ad8433014f5322635e1904b7
Isto <istobarton@gmail.com>
2015-05-28 16:26:23 -0500

    Pass getElementsByClassName and stringifyJSON tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+//Must use document.body, element.childNodes, and element.classList
+var getElementsByClassName = function(className){
+  htmlArray = [];
+  var dBody = document;
+  var classSearcher = function(element){
+  	var nodes = element.childNodes
+  	var length = nodes.length
+  	for(var i=0; i<length; i++){
+  		var classist = nodes[i].classList;
+  		if(classist!==undefined && classist.contains(className)){
+  			htmlArray.push(nodes[i]);
+  			}
+  		if(nodes[i].hasChildNodes()){
+  			classSearcher(nodes[i]);}
+  	}
+  }
+  classSearcher(dBody);
+  return htmlArray;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringy = "";
+  var inputType = typeof(obj);
+  if(inputType==="object"){
+  	if(Array.isArray(obj)){
+  		stringy = stringy + "[";
+  		var length = obj.length;
+  		if(length>0){
+	  		for(var i=0; i<length; i++){
+	  			if(typeof(obj[i])==="undefined" || typeof(obj[i])==="function"){
+            stringy = stringy
+	  			}else if(i===length-1){
+					  stringy = stringy + stringifyJSON(obj[i]) + "]";
+				  }else{
+					  stringy = stringy + stringifyJSON(obj[i]) + ",";
+				  }
+  			}
+  		}
+  		else{stringy = stringy + "]"}
+  	}
+   	else if(obj===null){
+   		stringy = stringy + "null";
+ 	  }
+   	else{
+   		stringy = stringy + "{";
+   		keyLength = Object.keys(obj).length
+   		for(var key in obj){
+   			if(typeof(key)==="undefined" || typeof(obj[key])==="undefined"){stringy = stringy}
+   			else if(typeof(key)==="function" || typeof(obj[key])==="function"){stringy = stringy}
+   			else if(keyLength>1){
+   				stringy = stringy + stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+   				keyLength--
+        }else{
+   				stringy = stringy + stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+   			}
+   		}
+   		stringy = stringy + "}"
+   	}
+  }
+  else if(inputType==="string"){
+  	stringy = stringy + '"' + obj + '"';
+  }
+  else if(inputType==="number" || inputType==="boolean"){
+  	stringMe = (obj).toString();
+  	stringy = stringy + stringMe.toString();
+  }
+  return stringy;
 };
